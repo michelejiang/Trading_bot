1f3695b060a27ef9eaa9f97cf90fb1e9907f867b
miche <michelej.jiang@gmail.com>
2021-08-02 22:50:53 +0200

    Prova

--- main.py
+++ main.py
@@ -28,12 +28,130 @@ def getValue(fromc, toc):
         datalist.append(tmp)
     return datalist
 
+def Monthlycalculate(datalist):
+    #Monthly, we get a generic view
+    point = 0.0
+    cmin = 10.0000
+    cmax = -1.0000
+    i = 0
+    s = 0
+    for candle in datalist[4]:
+        if i == 0: last = float(candle['4. close'])
+        if i == 4: first = float(candle['1. open'])
+        if i < 5: s += float(candle['4. close'])
+        cmin = min(cmin, float(candle['4. close']))
+        cmax = max(cmax, float(candle['4. close']))
+        i+=1
+    s /= 5
+    if first > last: point += 0.25
+    else: point -= 0.25
+    if s > last: point -= 0.15
+    else: point += 0.15
+    diff = cmax - cmin
+    if (last-cmin)/diff > 0.5: point += (last-cmin)/diff
+    else: point -= (last-cmin)/diff
+    return point
+
+def Weeklycalculate(datalist):
+    # Weekly, we get a generic view
+    point = 0.0
+    cmin = 10.0000
+    cmax = -1.0000
+    i = 0
+    s = 0
+    for candle in datalist[3]:
+        if i == 0: last = float(candle['4. close'])
+        if i == 4: first = float(candle['1. open'])
+        if i < 5: s += float(candle['4. close'])
+        cmin = min(cmin, float(candle['4. close']))
+        cmax = max(cmax, float(candle['4. close']))
+        i += 1
+    s /= 5
+    if first > last:
+        point += 0.50
+    else:
+        point -= 0.50
+    if s > last:
+        point -= 0.40
+    else:
+        point += 0.40
+    diff = cmax - cmin
+    if (last - cmin) / diff > 0.5:
+        point += 2*((last - cmin) / diff)
+    else:
+        point -= 2*((last - cmin) / diff)
+    return point
+
+def Dailycalculate(datalist):
+    # Daily, we get a generic view
+    point = 0.0
+    cmin = 10.0000
+    cmax = -1.0000
+    i = 0
+    s = 0
+    for candle in datalist[2]:
+        if i == 0: last = float(candle['4. close'])
+        if i == 4: first = float(candle['1. open'])
+        if i < 5: s += float(candle['4. close'])
+        cmin = min(cmin, float(candle['4. close']))
+        cmax = max(cmax, float(candle['4. close']))
+        i += 1
+    s /= 5
+    if first > last:
+        point += 0.70
+    else:
+        point -= 0.70
+    if s > last:
+        point -= 0.40
+    else:
+        point += 0.40
+    diff = cmax - cmin
+    if (last - cmin) / diff > 0.5:
+        point += 2 * ((last - cmin) / diff)
+    else:
+        point -= 2 * ((last - cmin) / diff)
+    return point
+
+def intradailycalculate(datalist):
+    point = 0.0
+    cmin = 10.0000
+    cmax = -1.0000
+    i = 0
+    s = 0
+    for candle in datalist[1]:
+        if i == 0: last = float(candle['4. close'])
+        if i == 4: first = float(candle['1. open'])
+        if i < 5: s += float(candle['4. close'])
+        cmin = min(cmin, float(candle['4. close']))
+        cmax = max(cmax, float(candle['4. close']))
+        i += 1
+    s /= 5
+    if first > last:
+        point += 0.10
+    else:
+        point -= 0.10
+    if s > last:
+        point -= 0.05
+    else:
+        point += 0.05
+    diff = cmax - cmin
+    if (last - cmin) / diff > 0.5:
+        point += 0.3 * ((last - cmin) / diff)
+    else:
+        point -= 0.3 * ((last - cmin) / diff)
+    return point
 
 def main():
     listExchange = [["USD", "EUR"], ["USD", "JPY"], ["USD", "CAD"], ["GBP", "USD"], ["USD", "CHF"], ["AUD", "USD"]]
     # test()
     for x in listExchange:
-        getValue(x[0], x[1])
+        score = 0
+        data = getValue(x[0], x[1])
+        score = Monthlycalculate(data)
+        score += Weeklycalculate(data)
+        score += Dailycalculate(data)
+        score += intradailycalculate(data)
+        print(score)
         time.sleep(61)
 
 
